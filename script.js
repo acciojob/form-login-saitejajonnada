7c28145857e53e8da0fd9c4f694111d3cefc0e74
Jonnada Sai Teja <146433770+saitejajonnada@users.noreply.github.com>
2026-04-16 18:33:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,16 @@
-<script>
-// Step 1: Access form
-const form = document.getElementById("nameForm");
+ 
+ const form = document.getElementById("nameForm");
 
-// Step 2: Add event listener
-form.addEventListener("submit", function(event) {
+ form.addEventListener("submit", function(event) {
 
-    // Step 3: Prevent page reload
-    event.preventDefault();
+     event.preventDefault();
 
-    // Step 4: Get first name
-    const fname = document.querySelector("#fname").value;
+     const fname = document.querySelector("#fname").value;
 
-    // Step 5: Get last name
-    const lname = document.querySelector("#lname").value;
+     const lname = document.querySelector("#lname").value;
 
-    // Step 6: Combine names
-    const fullName = fname + " " + lname;
+     const fullName = fname + " " + lname;
 
-    // Step 7: Show alert
-    alert(fullName);
+     alert(fullName);
 });
-</script>
+ 
